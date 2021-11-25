d52a99a1534520b4ea5f9f4b4f1e2414cd067734
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2021-11-25 10:43:11 -0300

    herenca

--- Program.cs
+++ Program.cs
@@ -11,15 +11,30 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
             Funcionario carlos = new Funcionario();
             carlos.Nome = "Carlos";
             carlos.CPF = "546.879.157-20";
             carlos.Salario = 2000;
 
+            gerenciador.Registrar(carlos);
 
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "532.123.321-10";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
 
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.Salario);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificacoes: R$ " + gerenciador.GetTotalBonificacao());
 
             Console.ReadLine();
         }
